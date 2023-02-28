584d696b0240be86f46d23a3bed974bb5d587bf2
Gabriel Oviedo <gabrieloviedo2017@outlook.com>
2023-02-28 10:52:42 -0300

    Desarrollo Web

---
+++ JS/function.js
@@ -0,0 +1,11 @@
+
+
+document.getElementById("telefono").onclick = function(){
+document.getElementById("telefono").innerHTML = "Teléfono: (+54) 236-4675962";
+}
+
+document.getElementById("Mail").onclick = function(){
+    document.getElementById("Mail").innerHTML = "Mail: gabrieloviedo2017@outlook.com ";
+    }
+
+ 
